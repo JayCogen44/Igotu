f87868dce77ee406c76fa10c5754b73718e7206a
Brisco <Sco-Codesmith@Kenneths-MacBook-Pro-2.local>
2019-01-22 20:25:28 -0800

    oAuth

--- server/server.js
+++ server/server.js
@@ -1,60 +1,101 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const http = require('http');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const http = require("http");
 
 const app = express();
 const port = 3000;
-const pg = require('pg');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const userController = require('./controllers/users-controller');
-const itemsController = require('./controllers/items-controller');
+const pg = require("pg");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const userController = require("./controllers/users-controller");
+const itemsController = require("./controllers/items-controller");
+const session = require("express-session");
+const passport = require("passport");
 
+//passport config
+require("./config/passport")(passport);
+
+//Load route
+const auth = require("./auth");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// express session
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+//=========================================================================
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-TypeError, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-TypeError, Accept"
+  );
   next();
 });
 
-app.get('/user/:email', (req, res, err) => {
+app.get("/login", (req, res) => {
+  res.status(200);
+  res.json({ what: "what" });
+});
+app.get("/user/:email", (req, res, err) => {
   // joins user table and item table
   res.status(200);
 });
 
-app.get('/item/:id', itemsController.getOneItem, (req, res, err) => {
+app.get("/item/:id", itemsController.getOneItem, (req, res, err) => {
   res.status(200).json(res.locals.oneItem);
 });
 
-app.get('/search/:item_name', itemsController.searchItem, (req, res, err) => {
+app.get("/search/:item_name", itemsController.searchItem, (req, res, err) => {
   res.status(200).json(res.locals.search);
 });
 
-app.get('/category/:category', itemsController.searchCategory, (req, res, err) => {
-  res.status(200).json(res.locals.category);
-});
+app.get(
+  "/category/:category",
+  itemsController.searchCategory,
+  (req, res, err) => {
+    res.status(200).json(res.locals.category);
+  }
+);
 
-app.get('/allItems', itemsController.getAllItems, (req, res, err) => {
+app.get("/allItems", itemsController.getAllItems, (req, res, err) => {
   res.status(200).json(res.locals.items);
 });
 
-app.post('/addUser', userController.addUser, (req, res, err) => {
+app.post("/addUser", userController.addUser, (req, res, err) => {
   res.status(200).json(res.locals.data);
 });
 
-app.post('/addItem', itemsController.addItem, (req, res, err) => {
+app.post("/addItem", itemsController.addItem, (req, res, err) => {
   res.status(200).json(res.locals.data);
 });
 
-app.delete('/deleteItem', (req, res, err) => {
+app.delete("/deleteItem", (req, res, err) => {
   // deletes item from database
   res.status(200);
 });
 
-app.use(express.static(path.resolve(__dirname, '../build')));
+app.use("/auth", auth);
+
+//=========================================================================\
+
+// app.use((req, res, next) => {
+//   res.locals.user = req.user || null;
+//   next();
+// });
+
+app.use(express.static(path.resolve(__dirname, "../build")));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
